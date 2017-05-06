25e15fba42143f39a0271cd41430d55dea94c3f0
nelsonic <contact.nelsonic+github@gmail.com>
2017-05-06 23:45:15 +0100

    adds tests for update function https://github.com/dwyl/learn-elm-architecture-in-plain-javascript/issues/3

---
+++ examples/counter-basic-test/test.js
@@ -0,0 +1,14 @@
+test('Test Update update(0) returns 0 (current state)', function(assert) {
+  var result = update(0);
+  assert.equal(result, 0);
+});
+
+test('Test Update increment: update(1, "inc") returns 2', function(assert) {
+  var result = update(1, "inc");
+  assert.equal(result, 2);
+});
+
+test('Test Update decrement: update(3, "dec") returns 2', function(assert) {
+  var result = update(1, "inc");
+  assert.equal(result, 2);
+});
